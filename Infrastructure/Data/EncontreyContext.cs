2a5e1f92b97ebcecf4fec7347c19566a74df873e
evertonbravo <everton.bravo@gmail.com>
2020-04-29 17:05:18 -0300

    Initial Commit

---
+++ Infrastructure/Data/EncontreyContext.cs
@@ -0,0 +1,36 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Reflection;
+
+namespace Infrastructure.Data
+{
+    public class EncontreyContext : DbContext
+    {
+        public EncontreyContext(DbContextOptions<EncontreyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            }
+        }
+       
+    }
+}
